500e84129ee3eed6d539e55f361ddf714dbddbc7
Fathiyahkhaq <kalamalhaqfathiyah@gmail.com>
2022-10-17 09:35:11 +0700

    tugas porto

---
+++ tugas/gen-11-tugas-tailwind-portfolio-FathiyahKalamalHaq/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        alkami: ['Alkalami', 'serif'],
+        quick: ['Quicksand', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif'],
+        kanit: ['Kanit','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
